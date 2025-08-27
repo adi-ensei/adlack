9f54fb2498db77fdbce4824f83ddb27af2d1b997
Adi <asanand1297@gmail.com>
2025-08-28 02:03:46 +0530

    started

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(ENV.PORT, () => {
+  console.log("Server started at on Port: ", ENV.PORT);
+});
