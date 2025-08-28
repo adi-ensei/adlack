00bac51204e720ccc8b53b0756c6017d80ee3694
adi-ensei <your_github_email@example.com>
2025-08-28 14:27:32 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,35 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/innjest.js";
 
 const app = express();
+app.use(express.json());
 
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.listen(ENV.PORT, () => {
-  console.log("Server started at on Port: ", ENV.PORT);
-  connectDB();
-});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server started at on Port: ", ENV.PORT);
+        connectDB();
+      });
+    }
+  } catch (error) {
+    console.log("Error Starting the server", error);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+export default app;
