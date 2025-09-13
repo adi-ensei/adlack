6c3126b1d3cd29002ef8284cc9c06777382cfb53
adi-ensei <your_github_email@example.com>
2025-09-13 13:36:11 +0530

    debuging for deployement

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -5,7 +5,7 @@ import axios from "axios";
 //     ? "http://localhost:5001/api"
 //     : "https://adlack.vercel.app/api";
 
-const BASE_URL = import.meta.VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
